9fb8a41ee87fca4991a364fbfd5e3e933a2df435
ghost <localhost>
2023-09-06 02:34:37 +0300

    add connection tip

--- src/public/action.php
+++ src/public/action.php
@@ -563,6 +563,9 @@ switch (isset($_GET['target']) ? urldecode($_GET['target']) : false)
 
           $response->message = sprintf( // @TODO MVC page for downloads needed
            '<h1 class="display-block margin-b-16 font-size-16">%s</h1>
+            <div class="margin-b-16 text-color-night">
+              * make sure BitTorrent client listening Yggdrasil interface!
+            </div>
             <a href="%s">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-magnet" viewBox="0 0 16 16">
                 <path d="M8 1a7 7 0 0 0-7 7v3h4V8a3 3 0 0 1 6 0v3h4V8a7 7 0 0 0-7-7Zm7 11h-4v3h4v-3ZM5 12H1v3h4v-3ZM0 8a8 8 0 1 1 16 0v8h-6V8a2 2 0 1 0-4 0v8H0V8Z"/>
@@ -823,7 +826,7 @@ switch (isset($_GET['target']) ? urldecode($_GET['target']) : false)
               <a class="button margin-l-8"
                   rel="nofollow"
                   href="<?php echo $_SERVER['HTTP_REFERER'] ?>">
-                <?php echo _('return') ?>
+                <?php echo _('back') ?>
               </a>
             </div>
           </div>
