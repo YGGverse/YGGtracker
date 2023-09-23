c4f5409ffa8c07712a77b58f30b275e04c45d883
ghost <localhost>
2023-09-23 21:31:43 +0300

    rename methods #15

--- src/app/controller/module/profile.php
+++ src/app/controller/module/profile.php
@@ -13,11 +13,11 @@ class AppControllerModuleProfile
   {
     $route = isset($_GET['_route_']) ? (string) $_GET['_route_'] : '';
 
-    $stars     = $this->_user->getUserPageStarsTotal();
-    $views     = $this->_user->getUserPageViewsTotal();
-    $downloads = $this->_user->getUserPageDownloadsTotal();
-    $comments  = $this->_user->getUserPageCommentsTotal();
-    $editions  = $this->_user->getUserPageEditionsTotal();
+    $stars     = $this->_user->findUserPageStarsDistinctTotalByValue(true);
+    $views     = $this->_user->findUserPageViewsDistinctTotal();
+    $downloads = $this->_user->findUserPageDownloadsDistinctTotal();
+    $comments  = $this->_user->findUserPageCommentsDistinctTotal();
+    $editions  = $this->_user->findUserPageEditionsDistinctTotal();
 
     include __DIR__ . '../../../view/theme/default/module/profile.phtml';
   }
