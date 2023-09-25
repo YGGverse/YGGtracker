bbd54b3a14a993b1f88c68df041195eeb2500047
ghost <localhost>
2023-09-25 16:02:25 +0300

    add content locale filter #14

--- src/app/controller/module/search.php
+++ src/app/controller/module/search.php
@@ -4,7 +4,20 @@ class AppControllerModuleSearch
 {
   public function render()
   {
-    $query = empty($_GET['query']) ? false : urldecode($_GET['query']);
+    $query  = empty($_GET['query']) ? false : urldecode($_GET['query']);
+    $locale = empty($_GET['locale']) ? 'all' : urldecode($_GET['locale']);
+
+    $locales = [];
+
+    foreach (Environment::config('locales') as $key => $value)
+    {
+      $locales[$key] = (object)
+      [
+        'key'    => $key,
+        'value'  => $value[0],
+        'active' => $key === $locale // false !== stripos($_SERVER['HTTP_ACCEPT_LANGUAGE'], $key) ? true : false,
+      ];
+    }
 
     include __DIR__ . '../../../view/theme/default/module/search.phtml';
   }
