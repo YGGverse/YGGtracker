0227f0c9ec9634ff56fcb2c375868cbc4d693f06
ghost <localhost>
2023-09-26 22:33:03 +0300

    implement request model

---
+++ src/app/model/request.php
@@ -0,0 +1,54 @@
+<?php
+
+class AppModelRequest {
+
+  private $_get;
+  private $_post;
+  private $_files;
+
+  public function __construct(array $get, array $post, array $files)
+  {
+    $this->_address = $address;
+    $this->_post    = $post;
+    $this->_files   = $files;
+  }
+
+  public function get(string $key) : mixed
+  {
+    if (isset($this->_get[$key]))
+    {
+      return $this->_get[$key];
+    }
+
+    else
+    {
+      return false;
+    }
+  }
+
+  public function post(string $key) : mixed
+  {
+    if (isset($this->_post[$key]))
+    {
+      return $this->_post[$key];
+    }
+
+    else
+    {
+      return false;
+    }
+  }
+
+  public function files(string $key) : mixed
+  {
+    if (isset($this->_files[$key]))
+    {
+      return $this->_files[$key];
+    }
+
+    else
+    {
+      return false;
+    }
+  }
+}
